d63de5c484f84834766c8a0d359a4e346f4550c7
Mary P <marylaboitebis@gmail.com>
2024-04-12 17:37:59 +0200

    tri des recettes par ingredient ou appareil ou ustensils

--- scripts/utils/displayUpdatedRecipes.js
+++ scripts/utils/displayUpdatedRecipes.js
@@ -6,9 +6,9 @@ import { recipes } from "../../data/recipes.js";
 
 
 /** Selection de plusieurs ingredients - selectSeveralIng */
-const selectionIng = [];
 const selectApp = [];
-const selectUte = [];
+const selectIng = [];
+const selectUst = [];
 
 export function displayUpdatedRecipes(name){
     displayRecipes(recipes);
@@ -16,13 +16,13 @@ export function displayUpdatedRecipes(name){
 }
 
 
-function createTag(name, elSelectedInList){
+function createTag(name, elSelected){
     const tag = document.createElement('div');
     tag.classList.add(`tag-${name}`);
 
     const textEl = document.createElement('p');
     textEl.classList.add(`tag-${name}-p`);
-    textEl.innerText =  `${elSelectedInList.innerText}`; 
+    textEl.innerText =  `${elSelected.innerText}`; 
 
     const crossToDeleteTag = document.createElement('span');
     crossToDeleteTag.classList.add('tag-delete');
@@ -49,19 +49,33 @@ function deleteTagAndUpdateList(tag, name){
     crossToDeleteTag.addEventListener('click', () => {
         const item = crossToDeleteTag.parentElement.querySelector('p').innerText.toLowerCase().trim();
         let  filteredRecipes = [];
+  
         /*****  update selection to the tag *****/
-        /** find index corresponding of item (innerText to the tag) */
-        const index = selectionIng.findIndex(i => i === item);
-         /** delete of the array selectionIng the item corresponding  */
-        selectionIng.splice(index, 1);
+        if(name === 'appliances'){
+            /** find index corresponding of item (innerText to the tag) */
+            const index = selectApp.findIndex(i => i === item);
+            /** delete of the array selectIng the item corresponding  */
+            selectApp.splice(index, 1);
+        }
+
+        if(name === 'ingredients'){
+            const index = selectIng.findIndex(i => i === item);
+            selectIng.splice(index, 1);
+        }
+
+        if(name === 'ustensils'){
+            const index = selectUst.findIndex(i => i === item);
+            selectUst.splice(index, 1);
+        }
 
         /** To delete tag */
         tag.style.display = 'none';
 
         /** To filter recipes */
-        filteredRecipes = getfilteredRecipes(recipes, name);
- 
-        putBackSelectedElFromList(filteredRecipes, selectionIng, name)
+        filteredRecipes = getFilteredRecipes(recipes, name);
+        /** put back selected element from list after delete tag corresponding */
+        putBackSelectedElFromList(filteredRecipes, selectIng, name);
+        putBackSelectedElFromList(filteredRecipes, selectUst, name);
 
         updateCounterRecipes();
 
@@ -87,8 +101,17 @@ function filterListElements(sortBy){
 }
 
 
+function getAppliances(recipe){
+    /** get appliance per recipe  */
+    let applianceByRecipes = [];
+    applianceByRecipes.push(recipe.appliance.toLowerCase().trim());
+
+    return applianceByRecipes;
+}
+
+
 function getIngredients(recipe){
-    /** get array of ingredients for each recipe */
+    /** get array of ingredients per recipe */
     const arrayIngredients = recipe.ingredients;
     let ingredientsByRecipes = [];
 
@@ -100,104 +123,103 @@ function getIngredients(recipe){
     return ingredientsByRecipes;
 }
 
-// function getAppliances(recipe){
-//     console.log(recipe.appliance);
-//     let applianceByRecipes = [];
-//     applianceByRecipes.push(recipe.appliance.toLowerCase().trim());
 
-//     return applianceByRecipes;
+function getUstensils(recipe){
+    /** get array of ingredients per recipe */
+    const arrayUstensils = recipe.ustensils;
+    let ustensilsByRecipes = [];
+
+    /** create an array of ingredients per recipe  */
+    arrayUstensils.forEach(ustensils => {         
+        ustensilsByRecipes.push(ustensils.toLowerCase().trim());
+    });
+
+    return ustensilsByRecipes;
+}
 
 
-
-// }
-
-
-
-
-function getfilteredRecipes(recipes, name){
+function getFilteredRecipes(recipes, name){
+    // console.log(selectApp);
+    // console.log(selectUst);
     /** list to return */
     const list = [];
     recipes.forEach(recipe => {
         let count = 0; 
 
+        // To filter by Appliances
+        if(name === 'appliances'){
+            let applianceByRecipes = getAppliances(recipe);
+            selectApp.forEach(app => {
+                /** displays recipes that contain the selected tag */
+                if(applianceByRecipes.indexOf(app) > -1){
+                    count ++;
+                }
+            });
+    
+            if( count == selectApp.length){
+                    /** Create a new recipe list when filtering by appliance selected */
+                list.push(recipe);
+            }
+        }
+
         if(name === 'ingredients'){
             let ingredientsByRecipes = getIngredients(recipe);
-            selectionIng.forEach(ing => {
+            selectIng.forEach(ing => {
                 /** displays recipes that contain the selected tag */
                 if(ingredientsByRecipes.indexOf(ing) > -1){
                     count ++;
                 }
-            })
+            });
     
-            if( count == selectionIng.length){
+            if( count == selectIng.length){
                 /** Create a new recipe list when filtering by ingredients selected */
                 list.push(recipe);
             }
         }
 
-
-
+        if(name === 'ustensils'){
+            let applianceByRecipes = getUstensils(recipe);
+            selectUst.forEach(ust => {
+                    /** displays recipes that contain the selected tag */
+                    if(applianceByRecipes.indexOf(ust) > -1){
+                        count ++;
+                    }
+                });
+        
+                if( count == selectUst.length){
+                     /** Create a new recipe list when filtering by appliance selected */
+                    list.push(recipe);
+                }
+            }
     });
     
-    return list; 
-      
-       
-
-
-
-
-     
-
-
-
-    // To filter by Appliances
-
-        // To filter by ingredients
-        // recipes.forEach(recipe => {
-        //     let count = 0; 
-
-        
-        //     let applianceByRecipes = getAppliances(recipe);
-     
-    
-        //     selectApp.forEach(app => {
-        //         /** displays recipes that contain the selected tag */
-        //         if(applianceByRecipes.indexOf(app) > -1){
-        //             count ++;
-        //         }
-        //     })
-    
-        //     if( count == selectApp.length){
-        //          /** Create a new recipe list when filtering by ingredients selected */
-        //         list.push(recipe);
-        //     }
-        // });
-
-    
+    return list;     
   
-    }
+}
 
 
-function removeSelectedElFromList(selectionIng, name){
+function removeSelectedElFromList(selectEl, name){
 
     const dropdown = document.querySelector(`#main_filter-bar-${name}`);
     const listElementsToFilter = document.querySelectorAll('.' + name);
+    console.log(listElementsToFilter);
 
    /** removes the displayed tag from the list of elements */
     listElementsToFilter.forEach(el => {
         // console.log(el);
-        selectionIng.forEach(selection => {
+        selectEl.forEach(selection => {
             // console.log(selection);
             if(el.innerText.toLowerCase().trim() === selection){
                 el.style.display = 'none';
             }
         });
     });
+
     dropdown.classList.toggle('displayBlock');
 }
 
 
-function putBackSelectedElFromList(filteredRecipes, selectionIng, name){
+function putBackSelectedElFromList(filteredRecipes, select, name){
 
     deleteListElement();
 
@@ -208,9 +230,8 @@ function putBackSelectedElFromList(filteredRecipes, selectionIng, name){
     const listElementsToFilter = document.querySelectorAll('.' + name);      
     /** removes the displayed tag from the list of elements */
     listElementsToFilter.forEach(el => {
-        // console.log(el);
-        selectionIng.forEach(selection => {
-            // console.log(selection);
+        select.forEach(selection => {
+            /** if the tag is in the updated list, we remove it */
             if(el.innerText.toLowerCase().trim() === selection){
                 el.style.display = 'none';
             }
@@ -233,34 +254,62 @@ function updateRecipesbyFilter(name, recipes){
 
     const mainFilter = document.querySelector(`#main_filter-${name}-wrapper`);
     const mainTagWrapper = mainFilter.querySelector('.main_Tag-wrapper');
-    let ingSelected;
+    let selection;
     const listElements = document.querySelectorAll('.' + name);
     let  filteredRecipes = [];
 
-    for(let elSelectedInList of listElements){
-        elSelectedInList.addEventListener('click', () => {
-            /** save ingredient selected */
-            ingSelected =  elSelectedInList.innerText.toLowerCase().trim(); 
-            /** save all selected ingredients*/
-            selectionIng.push(ingSelected);
+    for(let elSelected of listElements){
+        elSelected.addEventListener('click', () => {
+
+            /** save elements selected */
+            if(name === 'ingredients'){
+                selection =  elSelected.innerText.toLowerCase().trim(); 
+                selectIng.push(selection);
+            }
+            
+            if(name === 'appliances'){
+                selection =  elSelected.innerText.toLowerCase().trim(); 
+                selectApp.push(selection);
+            }
+
+            if(name === 'ustensils'){
+                selection =  elSelected.innerText.toLowerCase().trim(); 
+                selectUst.push(selection);
+            }
+
+
             /** Create tag  */
-            const tag = createTag(name, elSelectedInList);
+            const tag = createTag(name, elSelected);
             mainTagWrapper.appendChild(tag);
 
             /** delete list elements of filters for recreate it with updated recipes */
             deleteListElement();
         
-            filteredRecipes = getfilteredRecipes(recipes, name);
+            filteredRecipes = getFilteredRecipes(recipes, name);
+            console.log(filteredRecipes);
 
             filterListElements(filteredRecipes);
 
             displayRecipes(filteredRecipes);
 
-            removeSelectedElFromList(selectionIng, name);
+            /** remove elements in list of filter */
+            if(name === 'appliances'){
+                removeSelectedElFromList(selectApp, name);
+            }
+
+            if(name === 'ingredients'){
+                removeSelectedElFromList(selectIng, name);
+            }
+            
+            if(name === 'ustensils'){
+                removeSelectedElFromList(selectUst, name);
+            }
+    
             /** delete the tag and put the element back in the list */
             deleteTagAndUpdateList(tag, name);
 
             updateCounterRecipes();
+
         });       
     }
 
