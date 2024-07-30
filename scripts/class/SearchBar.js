b23904a1c2a1f62317770e5d270b34f6e3f78eac
Mary P <marylaboitebis@gmail.com>
2024-07-30 13:26:53 +0200

    boucle for

--- scripts/class/SearchBar.js
+++ scripts/class/SearchBar.js
@@ -55,45 +55,39 @@ export class SearchBar{
 
 
 
-
-    search(needle, recipes){
-
-        // const input = document.querySelector(`#search-q`);
-     
-        const removeAccents = str => str.normalize('NFD').replace(/[\u0300-\u036f]/g, ''); //remove accents and special characters 
+     search(needle, recipes) {
+        const removeAccents = str => str.normalize('NFD').replace(/[\u0300-\u036f]/g, ''); // remove accents and special characters 
         const research = removeAccents(needle);
         // this.recipesFiltered = [];  // delete previous results
-        
-
-        if (research.length < 3){
+    
+        if (research.length < 3) {
             console.log('La recherche doit comporter au moins 3 caractères');
             return recipes;
         }
-
-           const list = [];
-
-        recipes.forEach(recipe => {
-
+    
+        const list = [];
+    
+        for (let i = 0; i < recipes.length; i++) {
+            let recipe = recipes[i];
             let arrayIng = recipe.ingredients;
             let description = removeAccents(recipe.description.toLowerCase());
             let ing = [];
             let name = removeAccents(recipe.name.toLowerCase());
-
-
-              // Create an array of ingredients per recipe
-              arrayIng.forEach(ingredient => {
+    
+            // Create an array of ingredients per recipe
+            for (let j = 0; j < arrayIng.length; j++) {
+                let ingredient = arrayIng[j];
                 ing.push(removeAccents(ingredient.ingredient.toLowerCase()));
-            });
-
-            if ( ing.includes(research) || name.includes(research) || description.includes(research)) {
+            }
+    
+            if (ing.includes(research) || name.includes(research) || description.includes(research)) {
                 list.push(recipe);
-            } 
-            
-        });
-
+            }
+        }
+    
         return list;
-
     }
+    
 
 
     init(){
